a27cc58e5344bbc399a259d71f527881ef5e1d13
lcrispin <leonardo.crispin.oliveira00@gmail.com>
2024-04-06 15:18:15 -0300

    Create message queue

--- subscriberApi/src/main/java/br/com/newsletter/poc/subscriber/service/SubscriberService.java
+++ subscriberApi/src/main/java/br/com/newsletter/poc/subscriber/service/SubscriberService.java
@@ -1,17 +1,27 @@
 package br.com.newsletter.poc.subscriber.service;
 
+import br.com.newsletter.poc.subscriber.model.QueueMessage;
 import br.com.newsletter.poc.subscriber.model.SubscriberDto;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+@ApplicationScoped
 public class SubscriberService {
 
     @Inject
     private AmazonQueue subscriberQueue;
 
+    @ConfigProperty(name = "newsletter.topic.prefix", defaultValue = "test-")
+    private String topicPrefix;
+
     public void Subscribe(SubscriberDto subscriberDto) {
-        // TODO pegar topico de acordo com a newsllet parametrizada
-        // TODO criar mensagem da Queue
-        // TODO chamar metodo de Queue
+        String newsLetterTopicSubscribe = topicPrefix + subscriberDto.newsletter();
+
+        var queueMessage = new QueueMessage(subscriberDto.email(), newsLetterTopicSubscribe);
+
+
+        subscriberQueue.Send(queueMessage);
     }
 
 }
